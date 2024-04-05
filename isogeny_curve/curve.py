c7e5a2e674fd376e8b5571a9ce54fe59943c1b8e
Jamie-B-1 <jamiebell252@gmail.com>
2024-04-05 17:48:48 +0100

    Added function for separating parameters for A and B. Refactoring of module levels

--- isogeny_curve/curve.py
+++ isogeny_curve/curve.py
@@ -8,51 +8,21 @@ import cypari
 pari = cypari.pari
 
 
-class CurveStruct:
-
-    # initialise the curve structure
-    def __init__(self, l_a, e_a, l_b, e_b, cofactor, p, gen_fp2, elli_curve, P_1, Q_1, P_2, Q_2):
-        # l_a: small prime, e_a: exponent
-        self.l_a, self.e_a = l_a, e_a
-        # l_b: small prime, e_b: exponent
-        self.l_b, self.e_b = l_b, e_b
-        # cofactor ensures p is prime
-        self.cofactor = cofactor
-        # prime number of form: p = l_a^e_a * l_b^e_b * cofactor - 1
-        self.p = p
-        # generator of the field GF(p^2)
-        self.gen_fp2 = gen_fp2
-        # supersingular elliptic curve
-        self.elli_curve = elli_curve
-        # points to generate E[l_a^e_a]
-        self.P_1 = P_1
-        self.Q_1 = Q_1
-        # points to generate E[l_b^e_b]
-        self.P_2 = P_2
-        self.Q_2 = Q_2
-
-    # print the curve structure
-    def __str__(self):
-        return f"l_a: {self.l_a}, e_a: {self.e_a}\n" \
-                f"l_b: {self.l_b}, e_b: {self.e_b}\n" \
-                f"cofactor: {self.cofactor}\n" \
-                f"p: {self.p}\n" \
-                f"gen_fp2: {self.gen_fp2}\n" \
-                f"elli_curve: {self.elli_curve}\n" \
-                f"P_a: {self.P_1}\n" \
-                f"Q_a: {self.Q_1}\n" \
-                f"P_b: {self.P_2}\n" \
-                f"Q_b: {self.Q_2}\n"
-
-
 def generate_base_points(elli_curve):
-    P, Q = pari.ellgenerators(elli_curve)
-    return P, Q
+    points = pari.ellgenerators(elli_curve)
+    P_a = points[0][0]
+    Q_a = points[0][1]
+    P_b = points[1][0]
+    Q_b = points[1][1]
+    return P_a, Q_a, P_b, Q_b
 
 
-# walk through the steps of the isogeny
-def isogeny_walk():
-    return
+# create a dict to store base points and primes for A and B
+def create_params(elli_curve, l_a, e_a, l_b, e_b):
+    P_a, Q_a, P_b, Q_b = generate_base_points(elli_curve)
+    params = {"A": [P_a, Q_a, l_a, e_a],
+              "B": [P_b, Q_b, l_b, e_b]}
+    return params
 
 
 # create a supersingular elliptic starting curve
@@ -72,12 +42,47 @@ def create_curve(l_a, e_a, l_b, e_b, cofactor):
         print("Curve is not supersingular")
         return
     print(f"j-invariant: {str(elli_curve.j())}")
-    bases = generate_base_points(elli_curve)
-    P_1 = bases[0][0]
-    Q_1 = bases[0][1]
-    P_2 = bases[1][0]
-    Q_2 = bases[1][1]
-    return CurveStruct(l_a, e_a, l_b, e_b, cofactor, p, gen_fp2, elli_curve, P_1, Q_1, P_2, Q_2)
+
+    P_a, Q_a, P_b, Q_b = generate_base_points(elli_curve)
+
+    return CurveStruct(l_a, e_a, l_b, e_b, cofactor, p, gen_fp2, elli_curve, P_a, Q_a, P_b, Q_b)
+
+
+class CurveStruct:
+
+    # initialise the curve structure
+    def __init__(self, l_a, e_a, l_b, e_b, cofactor, p, gen_fp2, elli_curve, P_a, Q_a, P_b, Q_b):
+        # l_a: small prime, e_a: exponent
+        self.l_a, self.e_a = l_a, e_a
+        # l_b: small prime, e_b: exponent
+        self.l_b, self.e_b = l_b, e_b
+        # cofactor ensures p is prime
+        self.cofactor = cofactor
+        # prime number of form: p = l_a^e_a * l_b^e_b * cofactor - 1
+        self.p = p
+        # generator of the field GF(p^2)
+        self.gen_fp2 = gen_fp2
+        # supersingular elliptic curve
+        self.elli_curve = elli_curve
+        # points to generate E[l_a^e_a]
+        self.P_a = P_a
+        self.Q_a = Q_a
+        # points to generate E[l_b^e_b]
+        self.P_b = P_b
+        self.Q_b = Q_b
+
+    # print the curve structure
+    def __str__(self):
+        return f"l_a: {self.l_a}, e_a: {self.e_a}\n" \
+                f"l_b: {self.l_b}, e_b: {self.e_b}\n" \
+                f"cofactor: {self.cofactor}\n" \
+                f"p: {self.p}\n" \
+                f"gen_fp2: {self.gen_fp2}\n" \
+                f"elli_curve: {self.elli_curve}\n" \
+                f"P_a: {self.P_a}\n" \
+                f"Q_a: {self.Q_a}\n" \
+                f"P_b: {self.P_b}\n" \
+                f"Q_b: {self.Q_b}\n"
 
 
 # create a supersingular elliptic curve
